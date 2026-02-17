5cf36e43831d9bb55336e647f94683b147779bfd
rejin425 <rer127251@gmail.com>
2026-02-17 23:11:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -231,9 +231,12 @@ def results():
 def logout():
     session.clear()
     return redirect('/')
+import os
 
-if __name__=="__main__":
-    app.run(debug=True)
+port = int(os.environ.get("PORT", 10000))
+app.run(host="0.0.0.0", port=port)
+
+
 
 
 
